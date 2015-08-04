bf02f0bbe73019ebab8897113b53a73098133f11
Ian Ngiaw <ian@u.nus.edu>
2015-08-05 02:53:38 +0800

    update script

---
+++ livesports247/script.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+import xbmc,urllib
+
+all_modules = ['https://github.com/ianngiaw/p2pparser/raw/master/livesports247/livesports247.tar.gz']
+
+for parser in all_modules:
+    xbmc.executebuiltin('XBMC.RunPlugin("plugin://plugin.video.p2p-streams/?mode=405&name=p2p&url=' + urllib.quote(parser) + '")')
+    xbmc.sleep(1000)
+
+xbmc.executebuiltin("Notification(%s,%s,%i,%s)" % ('P2P-Streams', "All parsers imported",1,''))
