dd1601f0ba80bdf158754bbdb6a140cf58b6c0f2
Ian Ngiaw <ian@u.nus.edu>
2015-08-05 11:29:13 +0800

    updated to run faster

--- livesports247/livesports247/main.py
+++ livesports247/livesports247/main.py
@@ -3,7 +3,7 @@
 """ 
 This plugin is 3rd party and not part of p2p-streams addon
 
-livesport247
+livesports247
 
 """
 import sys,os,requests
@@ -16,9 +16,12 @@ from peertopeerutils.pluginxbmc import *
 from peertopeerutils.directoryhandle import *
 
 base_url = 'http://pilkalive.weebly.com'
+invalid_links = ['/en.html', '/sports.html', '/others.html', '/es.html', '/pl.html', '/pt.html', '/ru--ua.html', '/fr.html', '/se--dk--no.html', '/rs--hr.html', '/it.html', '/nl.html', '/cz.html', '/other.html']
 
 def module_tree(name,url,iconimage,mode,parser,parserfunction):
     if not parserfunction: livesport_main()
+    elif parserfunction == "livesports_links": livesports_links(url)
+    elif parserfunction == "ttvnet_acestream": ttvnet_acestream(url)
 
 def livesport_main():
     try:
@@ -28,12 +31,30 @@ def livesport_main():
     if source:
         match = list(filter(lambda x: '.html' in x, re.compile('<a href="(.+?)"').findall(source)))
         for link in match:
-            new_source = clean(get_page_source(base_url + link))
-            new_match = re.compile('<iframe scrolling="no" frameborder="0" src="(.+?)"').findall(new_source)
+            if link in invalid_links:
+                continue
             name = link[1:][0:-5]
-            for iframelink in new_match:
-                true_source = get_page_source(iframelink)
-                player = re.compile('this.loadPlayer\("(.+?)"').findall(true_source)
-                for acehash in player:
-                    addDir(name, "acestream://"+acehash,1,"http://www.masterlin.ru/wp-content/uploads/2014/02/Ace-Stream-Logo.jpeg",1,False)
+            addDir(name,base_url+link,401,os.path.join(current_dir,"icon.png"),1,False,parser="livesports247",parserfunction="livesports_links")
     return
+
+def livesports_links(url):
+    try:
+        source = clean(get_page_source(url))
+    except:
+        source = ""
+    if source:
+        match = re.compile('<iframe scrolling="no" frameborder="0" src="(.+?)"').findall(source)
+        for iframelink in new_match:
+            addDir(iframelink,iframelink,401,os.path.join(current_dir,"icon.png"),1,False,parser="livesports247",parserfunction="ttvnet_acestream")
+
+
+def ttvnet_acestream(url):
+    try:
+        source = clean(get_page_source(url))
+    except:
+        source = ""
+    if source:
+        player = re.compile('this.loadPlayer\("(.+?)"').findall(source)
+        for acehash in player:
+            ace_id = "acestream://" + acehash
+            addDir(ace_id, ace_id,1,"http://www.masterlin.ru/wp-content/uploads/2014/02/Ace-Stream-Logo.jpeg",1,False)
