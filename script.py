75c631ab19fad2a50563819ab368de84c32660c8
Ian Ngiaw <ian@u.nus.edu>
2015-08-07 01:38:09 +0800

    updated script

--- script.py
+++ script.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import xbmc,urllib
 
-all_modules = ['https://github.com/ianngiaw/p2pparser/raw/master/livesports247/livesports247.tar.gz', 'https://github.com/ianngiaw/p2pparser/raw/master/live-sports.cf/live-sports.cf.tar.gz']
+all_modules = ['https://github.com/ianngiaw/p2pparser/raw/master/livesports247/livesports247.tar.gz', 'https://github.com/ianngiaw/p2pparser/raw/master/livesportscf/livesportscf.tar.gz']
 
 for parser in all_modules:
     xbmc.executebuiltin('XBMC.RunPlugin("plugin://plugin.video.p2p-streams/?mode=405&name=p2p&url=' + urllib.quote(parser) + '")')
